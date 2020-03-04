415814e7de378f65d3ab890e639251910fadb6a9
Dmitriy <dmitrijsazonov@Dmitrijs-MacBook-Pro.local>
2020-03-04 23:46:58 +0300

    fix: style fix

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -11,9 +11,11 @@ import (
 func main() {
 	currentTime := time.Now()
 	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Printf("current time: %s\n", currentTime.String())
 	fmt.Printf("exact time: %s\n", exactTime.String())
 }
