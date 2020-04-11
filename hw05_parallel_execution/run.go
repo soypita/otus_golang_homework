f044022933b3bddd69be9d674bc67b6c5c98aae1
Dmitriy <dmitrijsazonov@Dmitrijs-MacBook-Pro.local>
2020-04-11 22:59:55 +0300

    fix: refactor

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -23,6 +23,33 @@ func executeTask(wg *sync.WaitGroup, taskCh <-chan Task, resultCh chan<- error, 
 	}
 }
 
+func resultListener(errorLimit int, tasksNum int, resultCh <-chan error, errorCh chan<- bool) {
+	errorCounter := 0
+	doneCounter := 0
+
+	// Receive results
+	for {
+		select {
+		case res := <-resultCh:
+			if res != nil {
+				errorCounter++
+			}
+			if res == nil {
+				doneCounter++
+			}
+			if errorCounter >= errorLimit {
+				errorCh <- true
+				return
+			} else if doneCounter == tasksNum {
+				errorCh <- false
+				return
+			}
+		default:
+			continue
+		}
+	}
+}
+
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error { //nolint:gocritic
 	if len(tasks) == 0 || N == 0 {
@@ -33,10 +60,7 @@ func Run(tasks []Task, N int, M int) error { //nolint:gocritic
 	taskCh := make(chan Task, len(tasks))
 	resultCh := make(chan error, len(tasks))
 	quitCh := make(chan struct{})
-	errorCounter := 0
-	doneCounter := 0
-	isWorking := true
-	isErrorExceed := false
+	errorCh := make(chan bool)
 
 	// Start N workers
 	for i := 0; i < N; i++ {
@@ -49,26 +73,11 @@ func Run(tasks []Task, N int, M int) error { //nolint:gocritic
 		taskCh <- val
 	}
 
-	// Receive results
-	for isWorking { //nolint:gosimple
-		select {
-		case res := <-resultCh:
-			if res != nil {
-				errorCounter++
-			}
-			if res == nil {
-				doneCounter++
-			}
-			if errorCounter >= M {
-				isWorking = false
-				isErrorExceed = true
-			} else if doneCounter == len(tasks) {
-				isWorking = false
-			}
-		default:
-			continue
-		}
-	}
+	// Start listener
+	go resultListener(M, len(tasks), resultCh, errorCh)
+
+	// Receive result
+	isErrorExceed := <-errorCh
 
 	// Stop all workers
 	for i := 0; i < N; i++ {
