c2e62da57a346509311b3b6a9baa1dc7538c8866
Dmitriy <dmitrijsazonov@Dmitrijs-MacBook-Pro.local>
2020-05-09 16:03:04 +0300

    feat: hw done

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,76 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+)
+
+// Check somple case
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	baseDir := "testdata/env"
+	baseNumOfFiles := 4
+
+	t.Run("should correctrly read env vars", func(t *testing.T) {
+		envMap, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, baseNumOfFiles, len(envMap))
+	})
+
+	t.Run("should skip file with invalid name", func(t *testing.T) {
+		invalidFileName := "test="
+		tmpFile, _ := ioutil.TempFile(baseDir, invalidFileName)
+		defer os.Remove(tmpFile.Name())
+		envMap, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, baseNumOfFiles, len(envMap))
+	})
+
+	t.Run("should collect env vars from subdirectory", func(t *testing.T) {
+		tmpDirPath := filepath.Join(baseDir, "tmpDir")
+		tmpFileName := "tmpFile"
+		os.Mkdir(tmpDirPath, os.ModePerm)
+		defer os.Remove(tmpDirPath)
+		tmpFile, _ := ioutil.TempFile(tmpDirPath, tmpFileName)
+		defer os.Remove(tmpFile.Name())
+		ioutil.WriteFile(tmpFile.Name(), []byte("test_data"), os.ModePerm)
+		envMap, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, baseNumOfFiles+1, len(envMap))
+	})
+
+	t.Run("should collect empty env string from empty file", func(t *testing.T) {
+		fileName := "test"
+		tmpFile, _ := ioutil.TempFile(baseDir, fileName)
+		defer os.Remove(tmpFile.Name())
+		envMap, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, baseNumOfFiles+1, len(envMap))
+		assert.Equal(t, "", envMap[filepath.Base(tmpFile.Name())])
+	})
+
+	t.Run("should collect only first line for env var from file", func(t *testing.T) {
+		fileName := "test"
+		tmpFile, _ := ioutil.TempFile(baseDir, fileName)
+		defer os.Remove(tmpFile.Name())
+		ioutil.WriteFile(tmpFile.Name(), []byte("first line\nsecond line"), os.ModePerm)
+		envMap, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, baseNumOfFiles+1, len(envMap))
+		assert.Equal(t, "first line", envMap[filepath.Base(tmpFile.Name())])
+	})
+
+	t.Run("should remove whitespace characters from env var", func(t *testing.T) {
+		fileName := "test"
+		tmpFile, _ := ioutil.TempFile(baseDir, fileName)
+		defer os.Remove(tmpFile.Name())
+		ioutil.WriteFile(tmpFile.Name(), []byte("first line\t \t \nsecond line"), os.ModePerm)
+		envMap, err := ReadDir(baseDir)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, baseNumOfFiles+1, len(envMap))
+		assert.Equal(t, "first line", envMap[filepath.Base(tmpFile.Name())])
+	})
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,50 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	notFoundErrorCode = 127
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("should return non zero code for empty cmd slice", func(t *testing.T) {
+		var tmpCmd []string
+		var tmpEnv Environment
+		code := RunCmd(tmpCmd, tmpEnv)
+		assert.Equal(t, defaultErrorCode, code)
+		code = RunCmd(nil, tmpEnv)
+		assert.Equal(t, defaultErrorCode, code)
+	})
+	t.Run("should succesfully run cmd", func(t *testing.T) {
+		tmpCmd := []string{"echo"}
+		code := RunCmd(tmpCmd, nil)
+		assert.Equal(t, defaultSuccessCode, code)
+	})
+	t.Run("should return code from command", func(t *testing.T) {
+		tmpCmd := []string{"/bin/bash", "wrong_command_name"}
+		code := RunCmd(tmpCmd, nil)
+		assert.Equal(t, notFoundErrorCode, code)
+	})
+	t.Run("should succesfully set env variable during execution", func(t *testing.T) {
+		tmpCmd := []string{"echo"}
+		tmpEnv := Environment{"TEST": "test"}
+		code := RunCmd(tmpCmd, tmpEnv)
+		assert.Equal(t, defaultSuccessCode, code)
+		assert.Equal(t, "test", os.ExpandEnv("$TEST"))
+	})
+	t.Run("should unset env var if empty", func(t *testing.T) {
+		os.Setenv("TEST", "test")
+		tmpCmd := []string{"echo"}
+		tmpEnv := Environment{"TEST": ""}
+		code := RunCmd(tmpCmd, tmpEnv)
+		assert.Equal(t, defaultSuccessCode, code)
+		remEnv, isPresent := os.LookupEnv("$TEST")
+		assert.Equal(t, false, isPresent)
+		assert.Equal(t, "", remEnv)
+	})
 }
